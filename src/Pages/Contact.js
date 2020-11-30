843b11c8cd4c0f2ef054d6ab06ecc451523b7750
Erick Lumunge <lumungep12@gmail.com>
2020-11-30 09:40:05 +0300

    contact form and styling

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import Menu from '../Components/Menu';
 
 export default function Contact(){
     return(
-        <h1>You can Contact us Here</h1>
+        <div>
+            <Menu />
+            <br />
+            <br />
+            <br />
+            <br />
+            <h1 className="contactTitle">Get In Touch</h1>
+            <div className="contact">
+            
+            <div className="illustration">
+                <h1>Illustration here</h1>
+            </div>
+                <div className="contactForm">
+                    <form>
+                    <input type="text" placeholder="Your Name..." />
+                    <br/>
+                    <br/>
+                    <input type="email" placeholder="Your Email..." />
+                    <br/>
+                    <br/>
+                    <textarea rows="6" placeholder="Your Messsage"></textarea>
+                    <br/>
+                    <br/>
+                    <input type="submit" className="button" value="Send Message" />
+                    </form>                    
+                </div>
+            </div>
+            </div>
     );
 }
