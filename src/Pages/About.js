a53a468fd43db98183d34cb0f21865c2a3962cb3
Erick Lumunge <lumungep12@gmail.com>
2020-12-02 16:37:26 +0300

    about page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -17,32 +17,51 @@ export default function About(){
         <div className="aboutContent">
 
         <div className="mainAbout">
-        <h1><i class="fas fa-quote-left"></i> clean, non-gmo, gluten-free and plant based ingredients</h1>
+        <h1 className="motto"><i class="fas fa-quote-left"></i> clean, non-gmo, gluten-free and plant based ingredients</h1>
             <p>About the company</p>
             <hr/>
             <div className="about about1">
-                <div className="gif1">gify here</div>
-                <div>
-                    <h2>Convenience</h2>
-                    <p>text about convenience and how people would prefer creme cakes</p>
+                <div className="gifCover">
+                <div className="gif1"></div>
+                </div>
+                <div className="desc">
+                    <h1>Convenience</h1>
+                    <p>text about convenience and how people would prefer creme cakes
+                    text about convenience and how people would prefer creme cakes
+                    text about convenience and how people would prefer creme cakes
+                    text about convenience and how people would prefer creme cakes
+                    </p>
                 </div>
             </div>
             <div className="about about2">
-            <div className="gif2">gify here</div>
-                <div>
-                    <h2>Taste</h2>
-                    <p>text about convenience and how people would prefer creme cakes</p>
+            <div className="gifCover">
+            <div className="gif2"></div>
+            </div>
+                <div className="desc">
+                    <h1>Taste</h1>
+                    <p>text about convenience and how people would prefer creme cakes
+                    text about convenience and how people would prefer creme cakes
+                    text about convenience and how people would prefer creme cakes
+                    text about convenience and how people would prefer creme cakes
+                    </p>
                 </div>
             </div>
             <div className="about about1">
-            <div className="gif3">gify here</div>
-                <div>
-                    <h2>Warmth</h2>
-                    <p>text about convenience and how people would prefer creme cakes</p>
+            <div className="gifCover">
+            <div className="gif3"></div>
+            </div>
+                <div className="desc">
+                    <h1>Warmth</h1>
+                    <p>text about convenience and how people would prefer creme cakes
+                    text about convenience and how people would prefer creme cakes
+                    text about convenience and how people would prefer creme cakes
+                    text about convenience and how people would prefer creme cakes
+                    </p>
                 </div>
             </div>
         </div>
         <div className="founder">
+            <h1 class="aboutText">Get To know Our Founder <span>Bakery Owner</span></h1>
             <div>
             <p>My story begins with a question: How do I live a full life without regrets? 
                 A few things I’ve learned since beating cancer: Try new things. Work out. 
@@ -52,11 +71,12 @@ export default function About(){
                 most dairy, and anything I couldn’t pronounce. And replaced those foods with clean ones. 
                 In a very short amount of time, I felt more energetic, more alive, more me.</p>
                 </div>
-                <div>
-                    <p>image here</p>
+                <div className="image">
+                    <div className="userImage"></div>
                 </div>
         </div>
         <div className="community">
+            <h1>Join Our Community of Fellow Cake Lovers and Receive Free Recepies</h1>
             <form>
                 <div>
                     <input type="text" name="name" required placeholder="Your Name..." />
@@ -67,8 +87,9 @@ export default function About(){
                 <div>
                     <input type="text" name="number" required placeholder="Phone Number..." />
                 </div>
+                <br/>
                 <div>
-                    <button type="submit">Submit</button>
+                    <button className="aboutButton" type="submit">Submit</button>
                 </div>
             </form>
         </div>
