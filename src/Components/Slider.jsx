e90170f6f56966379cd44663bd96dd772b596db6
Erick Lumunge <lumungep12@gmail.com>
2021-02-06 21:41:19 +0300

    final changes

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -4,23 +4,24 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import slide1 from '../img/slide1.jpg';
 import slide2 from '../img/slide2.jpg';
 import slide3 from '../img/slide3.jpg';
+import '../App.css';
 
 
 export default function Slider(){
     return(
         <div className="slider-wrapper">
             <Carousel showArrows={true} infiniteLoop autoPlay>
-                <div>
+                <div className="slider">
                     <img src={slide1} alt=""/>
-                    <h1>Moist</h1>
+                    <p>Moist</p>
                 </div>
-                <div>
+                <div className="slider">
                     <img src={slide2} alt=""/>
-                    <h1>Sweet</h1>
+                    <p>Sweet</p>
                 </div>
-                <div>
+                <div className="slider">
                     <img src={slide3} alt=""/>
-                    <h1>Chocolatey</h1>
+                    <p>Chocolatey</p>
                 </div>
             </Carousel>
         </div>
